ed6e77ed3be1d4ab37576799989584b568d65a85
benjGam <mecopilive@gmail.com>
2026-08-20 11:28:43 +0200

    feat add process related components

--- Il2CppResolver/Program.cs
+++ Il2CppResolver/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Il2CppResolver;
+﻿using System.Diagnostics;
+using UnityIl2CppResolver.Native.Process;
+
+namespace Il2CppResolver;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
-        
+      
     }
 }
